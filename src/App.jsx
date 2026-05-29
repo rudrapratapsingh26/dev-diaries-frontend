20fb4a5c176df8108636c463ea345e53ae868add
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-05-29 18:05:45 +0530

    feat: add routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import SinglePost from "./pages/SinglePost";
+import MyPosts from "./pages/MyPosts";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-blue-500">Dev Diaries</h1>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="max-w-4xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/posts/:slug" element={<SinglePost />} />
+          <Route
+            path="/my-posts"
+            element={
+              <ProtectedRoute>
+                <MyPosts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditPost />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
